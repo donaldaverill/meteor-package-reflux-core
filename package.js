d5ff6276b258e6df22fc3f3d5843262606ba6687
Donald Averill <donaldaverill@gmail.com>
2015-11-10 16:50:31 -0800

    fixing browserify

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'fourquet:reflux-core',
-  version: '0.3.0_1',
+  version: '0.3.0_2',
   summary: 'reflux-core wrapped for Meteor.',
   git: 'https://github.com/fourquet/meteor-package-reflux-core',
   documentation: 'README.md',
@@ -12,7 +12,7 @@ Npm.depends({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use('cosmos:browserify', 'client');
+  api.use('cosmos:browserify@0.8.3', 'client');
   api.addFiles('client.browserify.js', 'client');
   api.addFiles('require-reflux.js', 'server');
   api.export('Reflux');
